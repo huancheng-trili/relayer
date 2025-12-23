be8cc981e06811e4c8b2606d16a3a8c66b34e84f
Huan-Cheng Chang <huancheng.chang@trili.tech>
2025-12-23 14:45:17 +0000

    relay to tezlink

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
 import { ethers } from "ethers";
 import fs from 'fs';
+import { TezosToolkit } from '@taquito/taquito';
+import { InMemorySigner } from '@taquito/signer';
+
+async function forwardToTezlink(data) {
+    const tezlinkContract = await Tezos.contract.at(tezlinkAddress);
+
+    console.log(`sending data to tezlink`);
+    const operation = await tezlinkContract.methodsObject.default(data).send();
+
+    console.log(`Waiting for ${operation.hash} to be confirmed...`);
+    const level = await operation.confirmation(1);
+
+    console.log(`Operation injected at level ${level}: https://shadownet.tezlink.tzkt.io/${operation.hash}`);
+}
+
+async function forwardToEtherlink(data) {
+    const etherlinkContract = new ethers.Contract(etherlinkAddress, etherlinkAbi, etherlinkWallet);
+
+    let txt = await etherlinkContract.newIntent(data);
+    console.log("Transaction sent:", tx.hash);
+}
 
 const abi = JSON.parse(fs.readFileSync('dummy.abi', 'utf8'));
 const args = process.argv.slice(2);
-const contractAddress = args[0];
+const gatewayContractAddress = args[0];
 const provider = new ethers.WebSocketProvider("wss://base-sepolia-rpc.publicnode.com");
 
-const contract = new ethers.Contract(contractAddress, abi, provider);
+const gatewayContract = new ethers.Contract(gatewayContractAddress, abi, provider);
 
 const etherlinkAddress = "0x374a898D30E0447409e112F48715D722aF4D56CA";
 const etherlinkProvider = new ethers.JsonRpcProvider("https://node.ghostnet.etherlink.com");
-const etherlinkWallet = new ethers.Wallet(process.env.ETHERLINK_PRIVATE_KEY, etherlinkProvider);
+let etherlinkWallet;
+if (process.env.ETHERLINK_PRIVATE_KEY !== undefined) {
+    etherlinkWallet = new ethers.Wallet(process.env.ETHERLINK_PRIVATE_KEY, etherlinkProvider);
+}
 const etherlinkAbi = [
     "function newIntent(bytes calldata data) public",
 ];
 
-const etherlinkContract = new ethers.Contract(etherlinkAddress, etherlinkAbi, etherlinkWallet);
+const Tezos = new TezosToolkit('https://rpc.shadownet.tezlink.nomadic-labs.com');
+Tezos.setProvider({
+  signer: new InMemorySigner(process.env.TEZLINK_PRIVATE_KEY),
+});
+const tezlinkAddress = "KT1UGnBdWFNEftRhaKKbhYQhmSEczaVUWZZ1";
 
-contract.on("IntentSubmissionEvent", (a, b, c, d) => {
+gatewayContract.on("IntentSubmissionEvent", async (a, b, c, d) => {
     console.log(`data: ${a} ${b} ${c} ${d}`);
     const data = ethers.hexlify(ethers.toUtf8Bytes(`data: ${a} ${b} ${c} ${d}`));
-    etherlinkContract.newIntent(data).then((tx) => {
-        console.log("Transaction sent:", tx.hash);
-    }).catch(e => console.error(e));
+    await forwardToTezlink(data);
 });
